df8652019b21d96fc5b65f2d5903d9e02d314f09
Brion <40612857+Drazzot@users.noreply.github.com>
2024-11-14 17:32:27 -0500

    lots of changes

--- debugPointerArrrayProject.cpp
+++ debugPointerArrrayProject.cpp
@@ -1,80 +1,57 @@
 #include <iostream>
-
 #include <iomanip>
+#include <fstream>
 #include <vector>
 
 using namespace std;
 
 //Constants
-int const columns = 6;
-int const ARRAY_SIZE = 7;
+int const COLUMNS = 6;
+int const ARRAY_SIZE = 10;
 double const STATE_TAX = .04;
 double const FED_TAX = .05;
 double const FICA = .05;
-string const empInfoType[7] = { "", "ID","Gross Pay","State Tax","Federal Tax","FICA","Net Pay" };
+string const empInfoType[COLUMNS] = {"ID","Gross Pay","State Tax","Federal Tax","FICA","Net Pay" };
 
 //arrays
-double employeeInfo[10][6]; //2D array to hold up to 10 employee records
+double employeeInfo[ARRAY_SIZE][COLUMNS]; //2D array to hold up to 10 employee records
 
 //function prototypes
-void getEmployeeInfo(double [][columns], int);
+void openFile();
+void getSaveEmployeeRecords(double [][COLUMNS], int &);
+void getEmployeeInfo(double [][COLUMNS], int &);
 int getID();
-void computePay(double [], int);
-
-void DisplayEmployeeTable(double employees[], int ARRAY_SIZE);
-
-
-
-
-
-
-
+double computePay(double [][COLUMNS], int);
 double GetGrossPay();
-
-
+void displayEmployeeTable(double [][COLUMNS], int);
+void saveEmployeeInfo(double [][COLUMNS], int);
 void SetFormat(int precision, int width);
+void closeFile();
 
-void PopulateEmployeeTable(double employees[], double empInfo[], int  ARRAY_SIZE);
+//global
+fstream employeeSavedRecords;
 
-int main()
-
-{
-    //vector <double*> Employees;
-    //double empInfo[7];
-    //double* ptr;
-
-    //ptr = GetEmployeeInfo();
-    while (ptr[1] != 0)
-    {
-        Employees.push_back(ptr);
-        DisplayEmployeeTable(ptr, 7);
-        ptr = getEmployeeInfo(employeeInfo, 6);
-    }
-
-    // for (auto Employee : Employees)
-   //  {
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 7; j++)
-        {
-            cout << endl;
-            cout << empInfoType[j] << "\t" << Employees[i][j];
-        }
-    }
-    //  }
-
+int main() {
+    
+    
+    openFile();
+    int records = 0;
+    getEmployeeInfo(employeeInfo, records);
+    displayEmployeeTable(employeeInfo, records);
+    saveEmployeeInfo(employeeInfo, records);
+    closeFile();
 }
-void DisplayEmployeeTable(double employees[], int ARRAY_SIZE) {
 
-    double  empInfo = employees[0];
-    cout << left << empInfoType[1] << ": " << employees[1] << endl;
-    SetFormat(2, 15);
-    double grossPay = employees[1];
-    for (int i = 2; i < 7; i++)
-    {
-        cout << empInfoType[i] << ":\t$" << right << employees[i] << "\n";
+void displayEmployeeTable(double employeeInfo[][COLUMNS], int records) {
+    SetFormat(2, 3);
+    for (int i = 0; i < records; i++) {
+        cout << left << "ID " << left << static_cast<int>(employeeInfo[i][0]) << "\n";
+        for (int j = 1; j < COLUMNS; j++) {
+            SetFormat(2, 10);
+            cout << left << empInfoType[j] << ":\t$" << left << employeeInfo[i][j] << "\n";
+        }
+        cout << endl;
     }
-
 }
 
 void SetFormat(int precision, int width) {
@@ -87,107 +64,90 @@ void SetFormat(int precision, int width) {
 
 //         save in file and read in file
 
-void getEmployeeInfo(double employee[][columns], int rows) {
-    int count = 0;
-    while (count < rows) {
-        employee[count][0] = getID();
-        if (employee[count][0] > 0) {
-            employee[count][1] = GetGrossPay();
-            employee[count][2] = STATE_TAX;
-            employee[count][3] = FED_TAX;
-            employee[count][4] = FICA;
-            computePay(employeeInfo, count);
-            //double* ptr2 = EmployeeInfo;
-
+void getEmployeeInfo(double employee[][COLUMNS], int &records) {
+    getSaveEmployeeRecords();
+    int rowIndex = 0;
+    do {
+        employee[rowIndex][0] = getID();
+        if (employee[rowIndex][0] > 0) {
+            double grossPay = GetGrossPay();
+            employee[rowIndex][1] = grossPay;
+            employee[rowIndex][2] = grossPay * STATE_TAX;
+            employee[rowIndex][3] = grossPay * FED_TAX;
+            employee[rowIndex][4] = grossPay * FICA;
+            employee[rowIndex][5] = computePay(employeeInfo, rowIndex);
+            records++;
+            rowIndex++;
         }
-    }
-
-    
-    
+        else {
+            cout << "Done entering new records.\n\n";
+            break;
+        }
+    } while (rowIndex < ARRAY_SIZE);
 }
 
 int getID() {
     int id;
-    cout << "Please Enter the Employee ID:";
+    cout << "Please Enter the Employee ID: ";
     cin >> id;
     return id;
 }
 
-
-void computePay(double info[][columns], int row)
-
-{
-    double grossPay = info[row][1];
-    double netPay = grossPay;
-    for (int i = 2; i < size; i++)
-    {
-        info[row][i] = info[row][i] * grossPay;
-        netPay = netPay - info[row][i];
-    }
-    info[row][size - 1] = netPay;
-}
-
 double GetGrossPay() {
     double gp;
-    cout << "Please Enter the Employee's Gross Pay:";
+    cout << "Please Enter the Employee's Gross Pay: ";
     cin >> gp;
     return gp;
 }
 
+double computePay(double info[][COLUMNS], int row)
 
-
-void PopulateEmployeeTable(double employees[], double empInfo[], int  ARRAY_SIZE) {
-    static int EmployeeCount = 0;
-    employees[EmployeeCount] = *empInfo;
-    EmployeeCount += 1;
+{
+    double grossPay = info[row][1];
+    double deductions = 0.0;
+    for (int i = 2; i < 5; i++)
+    {
+        deductions += info[row][i];
+    }
+    return grossPay - deductions;
 }
 
-
-
-/*double GetStateTax() {
-
-    double st;
-
-    cout << "Please Enter the State tax percentage:";
-
-    cin >> st;
-
-    return st;
-
-
-
+void getSaveEmployeeRecords(double employee[][COLUMNS], int &records) {
+    while (employeeSavedRecords >> )
+    
+    for (int i = 0; i < records; i++) {
+        employeeSavedRecords << employeeInfo[i][0] << "\t";
+        for (int j = 1; j < COLUMNS; j++) {
+            employeeSavedRecords << employeeInfo[i][j] << "\t";
+        }
+        employeeSavedRecords << endl;
+    }
 }
 
-double GetFedTax() {
-
-    double ft;
-
-    cout << "Please Enter the Federal tax percentage:";
-
-    cin >> ft;
-
-    return ft;
-
-
-
+void saveEmployeeInfo(double[][COLUMNS], int records) {
+    for (int i = 0; i < records; i++) {
+        employeeSavedRecords << employeeInfo[i][0] << "\t";
+        for (int j = 1; j < COLUMNS; j++) {
+            employeeSavedRecords << employeeInfo[i][j] << "\t";
+        }
+        employeeSavedRecords << endl;
+    }
 }
 
-
-
-
-
-double GetFICA() {
-
-    double FICA;
-
-    cout << "Please Enter the Employee's FICA withholding:";
-
-    cin >> FICA;
-
-    return FICA;
-
-
-
+void openFile() {
+    employeeSavedRecords.open("employeeSavedRecords.txt", ios::app);
+        if (employeeSavedRecords) {
+            cout << "file open.\n";
+        }
+        else {
+            cout << "The file was not found or could not be opened. \n";
+            cout << "Please check to see if the file is in the correct folder and rerun the program. Exiting ...\n";
+            exit(0);
+        }
 }
 
-*/
+void closeFile() {
+    cout << "Closing file ...";
+    employeeSavedRecords.close();
+    cout << "File closed.";
+}
