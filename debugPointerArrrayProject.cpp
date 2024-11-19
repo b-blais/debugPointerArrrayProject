70c224b5619a17f9dcab2d8256d7118433b03f90
Brion Blais <brion.blais@outlook.com>
2024-11-19 09:12:09 -0500

    Added in a comment header block.

--- debugPointerArrrayProject.cpp
+++ debugPointerArrrayProject.cpp
@@ -1,3 +1,14 @@
+/*
+Title: Employee Records
+File Name : debugPointerArrayProject
+Programmer : Brion Blais
+Date : 11 / 2024
+Requirements :
+Write a program that will ask the user to enter an employee ID and Gross Wages.  Calculate the taxes and deductions to include take home pay and display the results.
+The program should store the information in a two dimenional array.
+The program should read previous records from a file and save the new records added.
+ */
+
 #include <iostream>
 #include <iomanip>
 #include <fstream>
